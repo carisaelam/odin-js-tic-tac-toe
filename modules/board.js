55a0c4bab681b636c1d3a1211629e6cb198c096c
carisaelam <carisaelam@gmail.com>
2024-10-06 12:37:15 -0400

    Add checkWin method and tests

--- modules/board.js
+++ modules/board.js
@@ -1,4 +1,4 @@
-// Builds, updates, prints board 
+// Builds, updates, prints board
 
 export default function board() {
   const BOARD_SIZE = 3;
@@ -51,5 +51,38 @@ export default function board() {
     return board;
   };
 
-  return { updateCell, clearBoard, printBoard };
+  // Check all horizontals for winning condition
+  const checkHorizontalWin = () => {
+    for (let i = 0; i < BOARD_SIZE; i++) {
+      if (board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+        return true;
+      } else return false;
+    }
+  };
+
+  // Check all verticals for winning condition
+  const checkVerticalWin = () => {
+    for (let i = 0; i < BOARD_SIZE; i++) {
+      if (board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+        return true;
+      } else return false;
+    }
+  };
+
+  // Check all diagonals for winning condition
+  const checkDiagonalWin = () => {
+    if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+      return true;
+    } else if (board[0][2] === board[1][1] && board[1][1] === board[0][2]) {
+      return true;
+    } else return false;
+  };
+
+  // Checks for all winning conditions
+  const checkWin = () => {
+    if (checkHorizontalWin() || checkVerticalWin() || checkDiagonalWin())
+      return true;
+  };
+
+  return { updateCell, clearBoard, printBoard, checkWin };
 }
--- tests/board.test.js
+++ tests/board.test.js
@@ -10,49 +10,90 @@ describe('Board', () => {
   });
 
   // updateCell tests
-  it('should update the given cell with the piece', () => {
-    const updatedBoard = gameBoard.updateCell([0, 0], 'X');
-    expect(updatedBoard[0][0]).toBe('X');
-  });
+  describe('updateCell functionality', () => {
+    it('should update the given cell with the piece', () => {
+      const updatedBoard = gameBoard.updateCell([0, 0], 'X');
+      expect(updatedBoard[0][0]).toBe('X');
+    });
 
-  it('should return an error for out-of-bounds coordinates', () => {
-    vi.spyOn(console, 'error');
-    const originalBoard = gameBoard.updateCell([3, 3], 'X');
-    expect(console.error).toHaveBeenCalledWith('not on board');
-    expect(originalBoard).toEqual([
-      ['_', '_', '_'],
-      ['_', '_', '_'],
-      ['_', '_', '_'],
-    ]);
-    console.error.mockRestore();
-  });
+    it('should return an error for out-of-bounds coordinates', () => {
+      vi.spyOn(console, 'error');
+      const originalBoard = gameBoard.updateCell([3, 3], 'X');
+      expect(console.error).toHaveBeenCalledWith('not on board');
+      expect(originalBoard).toEqual([
+        ['_', '_', '_'],
+        ['_', '_', '_'],
+        ['_', '_', '_'],
+      ]);
+      console.error.mockRestore();
+    });
 
-  it('should return an error for occupied cells', () => {
-    vi.spyOn(console, 'error');
-    gameBoard.updateCell([0, 0], 'X');
-    const updatedBoard = gameBoard.updateCell([0, 0], 'O');
-    expect(console.error).toHaveBeenCalledWith('already taken');
-    expect(updatedBoard).toEqual([
-      ['X', '_', '_'],
-      ['_', '_', '_'],
-      ['_', '_', '_'],
-    ]);
-    console.error.mockRestore();
+    it('should return an error for occupied cells', () => {
+      vi.spyOn(console, 'error');
+      gameBoard.updateCell([0, 0], 'X');
+      const updatedBoard = gameBoard.updateCell([0, 0], 'O');
+      expect(console.error).toHaveBeenCalledWith('already taken');
+      expect(updatedBoard).toEqual([
+        ['X', '_', '_'],
+        ['_', '_', '_'],
+        ['_', '_', '_'],
+      ]);
+      console.error.mockRestore();
+    });
   });
 
   // clearBoard tests
-  it('should clear the game board', () => {
-    gameBoard.updateCell([0, 0], 'X');
-    const clearedBoard = gameBoard.clearBoard();
-    expect(clearedBoard[0][0]).toEqual('_');
+  describe('clearBoard functionality', () => {
+    it('should clear the game board', () => {
+      gameBoard.updateCell([0, 0], 'X');
+      const clearedBoard = gameBoard.clearBoard();
+      expect(clearedBoard[0][0]).toEqual('_');
+    });
   });
 
   // printBoard tests
-  it('should print the current state of the board', () => {
-    gameBoard.updateCell([0, 0], 'X');
-    gameBoard.updateCell([0, 1], 'O');
-    gameBoard.updateCell([0, 2], 'O');
-    const printedBoard = gameBoard.printBoard();
-    expect(printedBoard[0]).toEqual('X | O | O');
+  describe('printBoard functionality', () => {
+    it('should print the current state of the board', () => {
+      gameBoard.updateCell([0, 0], 'X');
+      gameBoard.updateCell([0, 1], 'O');
+      gameBoard.updateCell([0, 2], 'O');
+      const printedBoard = gameBoard.printBoard();
+      expect(printedBoard[0]).toEqual('X | O | O');
+    });
+  });
+
+  // checkWin tests
+  describe('checkWin functionality', () => {
+    it('should return true for horizontal win', () => {
+      gameBoard.updateCell([0, 0], 'X');
+      gameBoard.updateCell([0, 1], 'X');
+      gameBoard.updateCell([0, 2], 'X');
+      expect(gameBoard.checkWin()).toBe(true);
+      gameBoard.clearBoard();
+    });
+
+    it('should return true for vertical win', () => {
+      gameBoard.updateCell([0, 0], 'X');
+      gameBoard.updateCell([1, 0], 'X');
+      gameBoard.updateCell([2, 0], 'X');
+      expect(gameBoard.checkWin()).toBe(true);
+      gameBoard.clearBoard();
+    });
+
+    it('should return true for negative slope diagonal win', () => {
+      gameBoard.updateCell([0, 0], 'X');
+      gameBoard.updateCell([1, 1], 'X');
+      gameBoard.updateCell([2, 2], 'X');
+      expect(gameBoard.checkWin()).toBe(true);
+      gameBoard.clearBoard();
+    });
+
+    it('should return true for positive slope diagonal win', () => {
+      gameBoard.updateCell([0, 2], 'X');
+      gameBoard.updateCell([1, 1], 'X');
+      gameBoard.updateCell([2, 0], 'X');
+      expect(gameBoard.checkWin()).toBe(true);
+      gameBoard.clearBoard();
+    });
   });
 });
