9eeb867fbcae882f5f2a30ac5fc9fe720eceb1db
carisaelam <carisaelam@gmail.com>
2024-10-06 09:46:05 -0400

    Update module syntax for import/export

---
+++ modules/board.js
@@ -0,0 +1,49 @@
+export default function board() {
+  const BOARD_SIZE = 3;
+
+  const board = [
+    ['_', '_', '_'],
+    ['_', '_', '_'],
+    ['_', '_', '_'],
+  ];
+
+  function printBoard() {
+    const printedBoard = [];
+    for (let i = 0; i < BOARD_SIZE; i++) {
+      const row = `${board[i][0]} | ${board[i][1]} | ${board[i][2]}`;
+      console.log(row);
+      printedBoard.push(row);
+    }
+    return printedBoard;
+  }
+
+  const updateCell = (coord, piece) => {
+    const row = coord[0];
+    const col = coord[1];
+
+    if (row > BOARD_SIZE - 1 || col > BOARD_SIZE - 1 || row < 0 || col < 0) {
+      console.error('not on board');
+      return board;
+    }
+
+    if (board[row][col] !== '_') {
+      console.error('already taken');
+      return board;
+    }
+
+    board[row][col] = piece;
+    return board;
+  };
+
+  const clearBoard = () => {
+    for (let i = 0; i < BOARD_SIZE; i++) {
+      for (let j = 0; j < BOARD_SIZE; j++) {
+        board[i][j] = '_';
+      }
+    }
+
+    return board;
+  };
+
+  return { updateCell, clearBoard, printBoard };
+}
